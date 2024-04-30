036c3007df6c732eb667f7d113c5da797869b26d
rawblocks69420 <ezramiller121@gmail.com>
2024-04-30 11:40:01 -0400

    more cleaning

--- quiz/App.js
+++ quiz/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, Button} from "react-native";
+import { View, StyleSheet, Text, Button, useWindowDimensions} from "react-native";
 import Swipeable from "./Swipeable";  
 import RenderHtml from "react-native-render-html";
-import { useWindowDimensions} from "react-native";
 import styles from "./styles";
 
 import { NavigationContainer } from "@react-navigation/native";
